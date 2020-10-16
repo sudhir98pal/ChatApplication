490fa5e7beb54aeafc8ec57cb38abeef992d0d84
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-10-16 13:27:33 +0530

    now multiple client can connected to socket

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,17 @@
-io()
+const socket=io();
+// accessing socket from sever on client side
+
+
+
+//listening to server by socket.on
+ socket.on('countUpdated',(count)=>
+ {
+     console.log('counthasbeen updated! to '+count);
+
+ })
+
+ document.querySelector("#increament").addEventListener('click',()=>
+ {
+     console.log('clicked');
+     socket.emit('increament')
+ })
--- src/startingpoint.js
+++ src/startingpoint.js
@@ -1,24 +1,33 @@
-const express=require('express');
-const http=require('http')
-const chalk=require('chalk');
-const app=express();
-const port=process.env.PORT||3000
-const path=require('path');
-const socketio=require('socket.io');
+const express = require('express');
+const http = require('http')
+const chalk = require('chalk');
+const app = express();
+const port = process.env.PORT || 3000
+const path = require('path');
+const socketio = require('socket.io');
 
-const server=http.createServer(app);
-const io=socketio(server);// simply socketio() will not work thus we need socketio(server)
+const server = http.createServer(app);
+const io = socketio(server);// simply socketio() will not work thus we need socketio(server)
 // now our server supports websockets
 
-const publicDirectoryPath=path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath));
-server.listen(port,()=>
-{  
+server.listen(port, () => {
     console.log(chalk.blueBright("Hi Sudhir Pal"))
-    console.log(chalk.greenBright.underline('Sever is Running on port '+port))
+    console.log(chalk.greenBright.underline('Sever is Running on port ' + port))
 })
 
-io.on('connection',()=>
-{
+let count = 0;
+io.on('connection', (socket) => {
     console.log('new websocket connection');
+    // socket.emit('countUpdated', count) it emits to single client
+    // it emits to every single client connected to this socket
+
+    socket.on('increament', () => {
+        
+    
+        count++;
+        console.log('counthasbeen updated! to ' + count);
+        io.emit('countUpdated', count)
+    })
 })
