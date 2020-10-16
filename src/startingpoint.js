9c29919e77c039f66cc9cbe3f0b5a045514dcabf
Sudhir Pal <sudhirnandlalpal@gmail.com>
2020-10-16 10:09:42 +0530

    now websocket connected to the server

--- src/startingpoint.js
+++ src/startingpoint.js
@@ -1,12 +1,24 @@
 const express=require('express');
+const http=require('http')
 const chalk=require('chalk');
 const app=express();
 const port=process.env.PORT||3000
 const path=require('path');
+const socketio=require('socket.io');
+
+const server=http.createServer(app);
+const io=socketio(server);// simply socketio() will not work thus we need socketio(server)
+// now our server supports websockets
+
 const publicDirectoryPath=path.join(__dirname,'../public')
 app.use(express.static(publicDirectoryPath));
-app.listen(port,()=>
-{
+server.listen(port,()=>
+{  
     console.log(chalk.blueBright("Hi Sudhir Pal"))
     console.log(chalk.greenBright.underline('Sever is Running on port '+port))
-});
+})
+
+io.on('connection',()=>
+{
+    console.log('new websocket connection');
+})
